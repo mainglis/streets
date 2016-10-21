902042c6f552faa7a62a0871aa119b9739741c5a
mainglis <maryashton.inglis@gmail.com>
2016-10-20 22:22:40 -0400

    well this works!

--- get_lat_longs.py
+++ get_lat_longs.py
@@ -9,25 +9,82 @@ from xml.etree import ElementTree as ET
 here_app_id = os.environ['HERE_APP_ID']
 here_app_code = os.environ['HERE_APP_CODE']
 here_base_url = 'https://geocoder.cit.api.here.com/6.2/geocode.xml?searchtext='
-here_gen = 9
+here_gen = '9'
+city = 'Boston'
 
-x = requests.get('https://geocoder.cit.api.here.com/6.2/geocode.xml?searchtext=Pine+St+@+Market+St+San+Francisco&app_id=' + here_app_id + '&app_code=' + here_app_code + '&gen=' + here_gen)
+def find_lats_longs(record):
+    if record[2] != 'Street':
+        main_street = record[2].replace(" ", "+")
+        cross_street_1 = record[3].replace(" ", "+")
+        cross_street_2 = record[4].replace(" ", "+")
+        intersection_1 = main_street + '@' + cross_street_1 + '+' + city
+        intersection_2 = main_street + '@' + cross_street_2 + '+' + city
 
-latitude = ET.fromstring(x.text).find('Response/View/Result/Location/DisplayPosition/Latitude')
-longitude = ET.fromstring(x.text).find('Response/View/Result/Location/DisplayPosition/Longitude')
+        x = requests.get(
+            'https://geocoder.cit.api.here.com/6.2/geocode.xml?searchtext=' + intersection_1 + '&app_id=' + here_app_id + '&app_code=' + here_app_code + '&gen=' + here_gen)
+        y = requests.get(
+            'https://geocoder.cit.api.here.com/6.2/geocode.xml?searchtext=' + intersection_2 + '&app_id=' + here_app_id + '&app_code=' + here_app_code + '&gen=' + here_gen)
 
-if latitude is not None:
-    print 'Found lat:', latitude.text
-else:
-    print 'nada'
+        int_1_latitude = ET.fromstring(x.text).find('Response/View/Result/Location/DisplayPosition/Latitude').text
+        int_1_longitude = ET.fromstring(x.text).find('Response/View/Result/Location/DisplayPosition/Longitude').text
+        int_2_latitude = ET.fromstring(y.text).find('Response/View/Result/Location/DisplayPosition/Latitude').text
+        int_2_longitude = ET.fromstring(y.text).find('Response/View/Result/Location/DisplayPosition/Longitude').text
 
-if longitude is not None:
-    print "found long:", longitude.text
+        record.extend([int_1_latitude, int_1_longitude, int_2_latitude, int_2_longitude])
+        return record
+    else:
+        record.extend(['start_lat', 'start_long', 'stop_lat', 'stop_long'])
+        return record
+
+with open('10_Long_Streets.csv', "r") as csvinput:
+
+    data = [row for row in csv.reader(csvinput.read().splitlines())]
+    data = [find_lats_longs(x) for x in data]
+
+    with open('output.csv', "w") as csvoutput:
+        writer = csv.writer(csvoutput, lineterminator='\n')
+        reader = iter(data)
+        all = []
+        row = next(reader)
+        all.append(row)
+
+        for row in reader:
+            all.append(row)
+        writer.writerows(all)
 
 
+
+
+
+#
 # with open('Street_Sweeping_Schedules.csv')as f:
 #     csv_f = csv.reader(f)
+#
+#     for row in csv_f:
+#         main_street = row[2].replace(" ", "+")
+#         cross_street_1 = row[3].replace(" ", "+")
+#         cross_street_2 = row[4].replace(" ", "+")
+#         intersection_1 = main_street + '@' + cross_street_1 + '+' + city
+#         intersection_2 = main_street + '@' + cross_street_1 + '+' + city
+#
+#         # return
+#
+#     x = requests.get(
+#         'https://geocoder.cit.api.here.com/6.2/geocode.xml?searchtext=' + intersection_1 + '&app_id=' + here_app_id + '&app_code=' + here_app_code + '&gen=' + here_gen)
+#     print 'result is ', x.text
 
-    # for row in csv_f:
-    #     print row[1]
-    # urllib2.urlopen(here_base_url).read()
+
+
+
+# x = requests.get('https://geocoder.cit.api.here.com/6.2/geocode.xml?searchtext=Pine+St+@+Market+St+San+Francisco&app_id=' + here_app_id + '&app_code=' + here_app_code + '&gen=' + here_gen)
+#
+# latitude = ET.fromstring(x.text).find('Response/View/Result/Location/DisplayPosition/Latitude')
+# longitude = ET.fromstring(x.text).find('Response/View/Result/Location/DisplayPosition/Longitude')
+#
+# if latitude is not None:
+#     print 'Found lat:', latitude.text
+# else:
+#     print 'nada'
+#
+# if longitude is not None:
+#     print "found long:", longitude.text
